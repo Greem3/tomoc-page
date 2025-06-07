e3d021a41364b824d11938bc8a87368cef1b9775
Ramerlin <ramerlint2008@gmail.com>
2025-06-07 18:47:08 -0400

    Updates Radix UI dependencies in package.json and pnpm-lock.yaml, including new components such as react-label, react-select and react-tabs. Modify layout.tsx to include MathJax settings and change the language to Spanish.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,14 +3,16 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header/index";
 import Footer from "@/components/Footer/Footer";
+import Script from "next/script";
+
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Tomoc",
-  description: "Tomoc es una app de matemáticas ",
+  title: "TOMOC App",
+  description: "Aplicación de olimpiadas matemáticas",
 };
 
 export default function RootLayout({
@@ -19,7 +21,36 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="es">
+      <head>
+        <Script
+          id="MathJax-config"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.MathJax = {
+                tex: {
+                  inlineMath: [['$', '$']],
+                  displayMath: [['$$', '$$']],
+                  packages: ['base', 'ams', 'noerrors', 'noundefined']
+                },
+                options: {
+                  ignoreHtmlClass: 'tex2jax_ignore',
+                  processHtmlClass: 'tex2jax_process'
+                },
+                startup: {
+                  typeset: false
+                }
+              };
+            `,
+          }}
+        />
+        <Script
+          id="MathJax-script"
+          src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"
+          strategy="beforeInteractive"
+        />
+      </head>
       <body
         className={`${inter.variable} antialiased`}
       >
