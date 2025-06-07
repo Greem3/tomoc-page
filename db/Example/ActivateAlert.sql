e13eb5798e5eba3e1d48f9a6151f674d28c179f7
Ian Pichardo <ian.pichardo@hotmail.com>
2025-06-06 22:13:19 -0400

    fix: Unnecesary files for the database

--- db/Example/ActivateAlert.sql
+++
@@ -1,7 +0,0 @@
-
-
-EXEC sp_MonitorDiskAndDataFiles
-    @DiskThresholdPercent = 100, -- Esto fuerza el disparo (ajústalo para pruebas)
-    @DataFileThresholdPercent = 100, -- También lo fuerza
-    @Recipients = 'ian.pichardo1@estudiante.edu.do',
-    @ProfileName = 'DBAOperador';
--- db/Example/KillProcess.sql
+++
@@ -1,6 +0,0 @@
-
-declare @kill int = 1;
-while @kill < 2
-begin
-    set nocount on;
-end
