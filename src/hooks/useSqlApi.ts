cefc5f78319a132ddcf825a487cb4b52ca460eb4
Ian Pichardo <ian.pichardo@hotmail.com>
2025-06-07 18:04:29 -0400

    docs: Warning

--- src/hooks/useSqlApi.ts
+++ src/hooks/useSqlApi.ts
@@ -2,6 +2,8 @@ import { Method } from "axios";
 import useJsqlApi, { IEntity } from "jsql-api";
 import _IQuery from "jsql-api/dist/objects/src/internal/abstract/IQuery";
 
+//! Por alguna razón esto hace que se deje de ejecutar el proyecto XD
+//! (No importa si tiene try catch, pasa lo mismo)
 export default async function useSqlApi<Query extends _IQuery, Table extends IEntity>(
     {
         path,
@@ -15,6 +17,19 @@ export default async function useSqlApi<Query extends _IQuery, Table extends IEn
         testMode: boolean
     }
 ) {
+    console.log({
+        path: path,
+        method: method,
+        body: body,
+        params: {
+            server: testMode ? 'localhost' : process.env.server,
+            database: process.env.databaseName,
+            uid: process.env.serverUid,
+            pwd: process.env.serverPwd,
+            encrypt: 'yes'
+        },
+        isLocal: testMode ? 8000 : undefined
+    })
 
     const response = await useJsqlApi<Query, Table>({
         path: path,
