f98b33c632701bb447716e1299d6347725f640c8
Ian Pichardo <ian.pichardo@hotmail.com>
2025-06-07 20:06:09 -0400

    refactor: Changed to LaTexText

--- src/app/problemas/_components/ProblemDetail.tsx
+++ src/app/problemas/_components/ProblemDetail.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Problem } from "./types"
+import { Problem, Solution } from "./types"
 import { Button } from "@/components/ui/button"
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
 import { ArrowLeft } from "lucide-react"
@@ -12,7 +12,7 @@ import { Textarea } from "@/components/ui/textarea"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 import { CheckCircle, XCircle, Plus, Send } from "lucide-react"
 import Link from "next/link"
-import LatexText from "./latex-text"
+import LatexText from "@/components/Texts/LaTexText"
 
 interface ProblemDetailProps {
     problem: Problem | null
