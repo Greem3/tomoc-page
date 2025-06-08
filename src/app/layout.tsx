e05b803f6e4d466dc8faee1703911969271ee7bd
Ian Pichardo <ian.pichardo@hotmail.com>
2025-06-07 20:06:38 -0400

    fix: Deleted Script and added again MathJaxContext

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Header from "@/components/Header/index";
 import Footer from "@/components/Footer/Footer";
 import Script from "next/script";
+import { MathJaxContext } from "better-react-mathjax";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -22,40 +23,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
-      <head>
-        <Script
-          id="MathJax-config"
-          strategy="beforeInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.MathJax = {
-                tex: {
-                  inlineMath: [['$', '$']],
-                  displayMath: [['$$', '$$']],
-                  packages: ['base', 'ams', 'noerrors', 'noundefined']
-                },
-                options: {
-                  ignoreHtmlClass: 'tex2jax_ignore',
-                  processHtmlClass: 'tex2jax_process'
-                },
-                startup: {
-                  typeset: false
-                }
-              };
-            `,
-          }}
-        />
-        <Script
-          id="MathJax-script"
-          src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"
-          strategy="beforeInteractive"
-        />
-      </head>
       <body
         className={`${inter.variable} antialiased`}
       >
         <Header />
-        {children}
+
+        <MathJaxContext>
+          {children}
+        </MathJaxContext>
+        
         <Footer />
       </body>
     </html>
