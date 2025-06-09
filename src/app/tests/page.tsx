b31191012712652dff19ed2727b2e7175aeba921
Ian Pichardo <ian.pichardo@hotmail.com>
2025-06-09 19:04:54 -0400

    feat: FINALLY API WORKS

--- src/app/tests/page.tsx
+++ src/app/tests/page.tsx
@@ -2,55 +2,11 @@
 
 import MarkdownEditor from "@/components/Editors/MarkdownEditor";
 import MarkdownLaTexEditor from "@/components/Editors/MarkdownLaTexEditor";
-import { fetchJsqlApi } from "@/functions/fetchJsqlApi";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export default function Tests() {
 
     const [text, setText] = useState('Initial Textx')
-    const [contests, setContests] = useState<any>()
-    const [isLoading, setIsLoading] = useState(true)
-
-    useEffect(() => {
-
-        const fetchContests = async () => {
-
-            const answer = await fetchJsqlApi({
-                path: 'select',
-                method: 'post',
-                params: {
-                    server: 'localhost',
-                    uid: 'ServerTomocAdmin',
-                    pwd: 'Tomoc@Db.2025',
-                    database: 'TomocDb'
-                },
-                body: {
-                    table: { name: 'VContests', sql_schema: 'content'}
-                },
-                isLocal: 8000
-            })
-
-            setContests(answer);
-            setIsLoading(false);
-        }
-
-        fetchContests()
-    }, [])
-
-    if (isLoading) {
-        return <p>Loading...</p>;
-    }
-
-    console.log(contests);
-
-    // useSqlApi({
-    //             path: 'select',
-    //             method: 'post',
-    //             body: {
-    //                 table: { name: 'VContests', sql_schema: 'content'}
-    //             },
-    //             testMode: true
-    //         })
 
     return <div className='w-full h-full flex flex-col gap-6 justify-center items-center align-center'>
         <MarkdownEditor
