288f37735cdddeb1747ea55a976ee73fbaa60078
Ian Pichardo <ian.pichardo@hotmail.com>
2025-06-11 18:59:53 -0400

    fix: dbo.VProblems don't exists

--- background/updates/updateProblems.ts
+++ background/updates/updateProblems.ts
@@ -3,14 +3,15 @@ import { ISelect } from 'jsql-api'
 import { editFile } from '../utils/writeFile'
 import IVProblems from '@/interfaces/Views/IVProblems'
 
-export async function updateProblemTypes() {
+export async function updateProblems() {
     
     const response = await useSqlApi<ISelect<IVProblems>, IVProblems[]>({
         path: 'select',
         method: 'post',
         body: {
             table: {
-                name: "VProblems"
+                name: "VProblems",
+                sql_schema: 'content'
             }
         }
     })
