5c6d7a1e1d137a8fef748ca78b276093675a87cc
Ian Pichardo <ian.pichardo@hotmail.com>
2025-06-12 10:17:56 -0400

    test: Deleted functionality from tests page

--- src/app/tests/page.tsx
+++ src/app/tests/page.tsx
@@ -2,42 +2,11 @@
 
 import MarkdownEditor from "@/components/Editors/MarkdownEditor";
 import MarkdownLaTexEditor from "@/components/Editors/MarkdownLaTexEditor";
-import useSqlApi from "@/hooks/useSqlApi";
-import { IProblemTypes } from "@/interfaces/Views/IProblemTypes";
-import { ISelect } from "jsql-api";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export default function Tests() {
 
     const [text, setText] = useState('Initial Textx')
-    const [problemTypes, setProblemTypes] = useState<IProblemTypes[]>()
-    const [isLoading, setIsLoading] = useState(true);
-
-    useEffect(() => {
-
-        const fetchTest = async () => {
-            const answer =  await useSqlApi<ISelect<IProblemTypes>, IProblemTypes[]>({
-                path: 'select',
-                method: 'post',
-                body: {
-                    table: {
-                        name: "ProblemTypes"
-                    }
-                }
-            })
-
-            setProblemTypes(answer?.data);
-            setIsLoading(false);
-        }
-
-        fetchTest();
-    }, [])
-
-    if (isLoading) {
-        return;
-    }
-
-    console.log(problemTypes);
 
     return <div className='w-full h-full flex flex-col gap-6 justify-center items-center align-center'>
         <MarkdownEditor
