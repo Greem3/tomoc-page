f08bca4849116874dab72796388207ddc11354a7
Ian Pichardo <ian.pichardo@hotmail.com>
2025-06-12 21:18:29 -0400

    del: Deleted Data Json Files
    This auto-generated by code

--- background/utils/editJsonFile.ts
+++ background/utils/editJsonFile.ts
@@ -3,7 +3,7 @@ import { __dirname } from "../main";
 import { Dictionary } from "../types/dictionary";
 import { writeFileSync } from "fs";
 
-export function editFile(path: string, data: Dictionary|Dictionary[]) {
+export function editJsonFile(path: string, data: Dictionary|Dictionary[]) {
     const outputPath = join(__dirname, path);
     writeFileSync(outputPath, JSON.stringify(data, null, 4))
 }
